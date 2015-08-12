a20ef58e9d2001721ccc0f92658df09edda5461a
Sebastien <sebastien.dabdoub@gmail.com>
2015-08-11 21:15:56 -0400

    Checks in partial work towards eval.
    I have implemented evaluation via a visitor pattern
    to handle the recursive nature of lisp expressions.
    
    I have implemented the special forms quote, if, and define.

--- lisp++.cpp
+++ lisp++.cpp
@@ -3,6 +3,10 @@
 #include "lispUtils.h"
 #include <boost/algorithm/string.hpp>
 
+///
+/// Parsing
+///
+
 // Tokenize the input string and fill tokens with it
 template<typename StrVec>
 void tokenize(StrVec& tokens, const std::string& inp) {
@@ -13,10 +17,10 @@ void tokenize(StrVec& tokens, const std::string& inp) {
   boost::replace_all(str,"("," ( ");
   boost::replace_all(str,")"," ) ");
   boost::trim(str);
-  
+
   // split by space (compress extra spaces)
-  boost::split(tokens, 
-               str, 
+  boost::split(tokens,
+               str,
                boost::is_any_of(" "),
                boost::token_compress_on);
 }
@@ -72,6 +76,106 @@ Expression parse(const std::string& program) {
 }
 
 
+///
+/// Evaluation
+///
+
+class eval_visitor: public boost::static_visitor<Expression>
+{
+public:
+  eval_visitor(Env& env) : m_env(env) {}
+
+  Expression operator()( const Symbol& sym ) const
+  {
+    // TODO: remove
+    std::cout << "symbol: " << sym << std::endl;
+    auto it = m_env.find(sym);
+    if (it == m_env.end()) {
+      // TODO: improve error message construction
+      return std::string("Could not find symbol '"+sym+"'");
+    }
+
+    return it->second;
+  }
+
+  Expression operator()( const Number& num ) const
+  {
+    // TODO: remove
+    std::cout << "number: " << num << std::endl;
+    return num;
+  }
+
+  Expression operator()( const Expression& expr ) const
+  {
+    // TODO: remove
+    std::cout << "expression: " << std::endl;
+    return boost::apply_visitor(*this,expr);
+  }
+
+  Expression operator()( const List& list ) const
+  {
+    // TODO: remove
+    std::cout << "list: " << std::endl;
+
+    // TODO: whats the right thing here?
+    if (list.size() == 0) {
+      return std::string("nil");
+    }
+
+    std::string proc = boost::get<std::string>(list[0]);
+
+    if (proc == "quote") {
+      Expression exp = list[1];      
+      return exp;
+    }
+
+    if (proc == "if") {
+      Expression test = list[1];
+      Expression conseq = list[2];
+      Expression alt = list[3];
+      
+      // TODO: refactor once tested
+      Expression out = eval(test,m_env);
+      Number t = boost::get<Number>(out);
+      int b = boost::get<int>(t);
+      if (b) {
+        return conseq;
+      } else {
+        return alt;
+      }
+
+    }
+
+    if (proc == "define") {
+      Symbol var = boost::get<Symbol>(list[1]);
+      Expression exp = list[2];
+      m_env[var] = exp;
+      return exp;
+    }
+
+    // TODO other special forms: apply, lambda?
+
+    // TODO: handle list correctly
+    return boost::apply_visitor(*this,list[0]);
+  }
+
+private:
+  Env& m_env;
+};
+
+Expression eval(const Expression& x, Env& env) {
+  eval_visitor v(env);
+  return boost::apply_visitor(v,x);
+}
+
+
+
+
+///
+/// REPL
+///
+
+
 int main() {
 
   auto program = "(foo (bar baz) (print hello world) (+ 1 1.5))";
@@ -80,6 +184,16 @@ int main() {
   print_visitor visitor;
   Expression ast = parse(program);
   std::cout << "ast: " << ::apply_visitor(visitor,ast) << std::endl;
+
+  Env env;
+  Expression out = eval(ast,env);
+  std::cout << "evaled: " << ::apply_visitor(visitor,out) << std::endl;
+
+  
+  out = eval(parse("(define r 0)"),env);
+  out = eval(parse("(if r 5 3)"),env);
+  std::cout << "evaled: " << ::apply_visitor(visitor,out) << std::endl;
+
 
   return 0;
 }
--- lisp++.h
+++ lisp++.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <map>
 #include <boost/variant.hpp>
 
 // define relevant types
@@ -9,9 +10,9 @@ typedef std::string Symbol;
 
 
 typedef boost::make_recursive_variant<Number, Symbol, std::vector< boost::recursive_variant_ > >::type Expression;
-
 typedef std::vector<Expression> List;
 
+typedef std::map<std::string,Expression> Env;
 
 // forward declare functions
 Expression atom(const std::string& val);
@@ -24,4 +25,5 @@ void tokenize(StrVec& tokens, const std::string& inp);
 
 Expression parse(const std::string& program);
 
-
+// TODO: define global default Env
+Expression eval(const Expression& x, Env& env);
